d5d66e87f400882a934619dc838c8fd8000e6a7a
Michael Halley-Frame <michael@halley-frame.com>
2025-11-01 18:24:49 +1100

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Michael Halley-Frame on 1/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
